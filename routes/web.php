4364355dbaa11e1b2e58653f646cee4131de3cd8
Christian Marcellino <christianmarcellino14@gmail.com>
2025-06-12 21:28:53 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/fakultas', FakultasController::class);
+    Route::resource('/prodi', ProdiController::class);
+    Route::resource('/mahasiswa', MahasiswaController::class);
+    Route::resource('/mata_kuliah', MataKuliahController::class);
+    Route::resource('/sesi', SesiController::class);
+    Route::resource('/jadwal', JadwalController::class);
+    Route::resource('/materi', MateriController::class);
+    Route::get('/dashboard',[DashboardController::class, 'index'])->middleware('verified')->name('dashboard');
 });
 
-Route::resource('/fakultas', FakultasController::class);
-Route::resource('/prodi', ProdiController::class);
-Route::resource('/mahasiswa', MahasiswaController::class);
-Route::resource('/mata_kuliah', MataKuliahController::class);
-Route::resource('/sesi', SesiController::class);
-Route::resource('/jadwal', JadwalController::class);
-Route::resource('/materi', MateriController::class);
-Route::get('/dashboard',[DashboardController::class, 'index'])->middleware(['auth','verified'])->name('dashboard');
+
 
 
 require __DIR__.'/auth.php';
